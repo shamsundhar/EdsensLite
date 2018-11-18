82a8f0bb84a3db2599e36a3d7f33102459f79441
syammanu <shyam.kdp@gmail.com>
2018-11-18 09:21:04 +0530

    messages UI

--- app/src/main/java/com/school/edsense_lite/messages/NewMessageFragment.java
+++ app/src/main/java/com/school/edsense_lite/messages/NewMessageFragment.java
@@ -1,19 +1,27 @@
 package com.school.edsense_lite.messages;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.school.edsense_lite.BaseFragment;
 import com.school.edsense_lite.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class NewMessageFragment extends BaseFragment {
 
 
-
+    @BindView(R.id.txtMsg)
+    EditText message;
+    @BindView(R.id.txtSub)
+    EditText subject;
+    @BindView(R.id.txtTo)
+    EditText to;
     public static NewMessageFragment newInstance() {
         NewMessageFragment fragment = new NewMessageFragment();
         return fragment;
@@ -31,9 +39,20 @@ public class NewMessageFragment extends BaseFragment {
 
         ButterKnife.bind(this, view);
         fragmentComponent().inject(this);
+        applyFonts();
 
 
         return view;
     }
+    private void applyFonts(){
+        // Font path
+        String fontPath = "fonts/bariol_bold-webfont.ttf";
+        // Loading Font Face
+        Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), fontPath);
 
+        to.setTypeface(tf);
+        subject.setTypeface(tf);
+        message.setTypeface(tf);
+
+    }
 }
